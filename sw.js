e1c65b8e9511c5823979ef5de0053f9069e49265
jaxx2104 <jaxx2104@gmail.com>
2017-07-18 02:40:40 +0900

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-3d95f5063e8213e7d194.js","99338e30207102734ed1f82069dad4a0"],["/commons-77de073d29acca3cf078.js","a1d76ac7fc44786569d84424012759b0"],["/index.html","4e22f2cf5b1f0ed216207e1c977fcb3a"],["/offline-plugin-app-shell-fallback/index.html","4d56462163c941039901237b55285064"]];
+var precacheConfig = [["/app-3d95f5063e8213e7d194.js","99338e30207102734ed1f82069dad4a0"],["/commons-77de073d29acca3cf078.js","a1d76ac7fc44786569d84424012759b0"],["/index.html","eab20f12506100b8036ccff9fc585977"],["/offline-plugin-app-shell-fallback/index.html","deb60b42112eb291819d1cef1e593f3f"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
